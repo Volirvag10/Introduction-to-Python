42ae5c7e447856b6d42882c5bb4b0ed7acd89e78
Gleb.Gavrilov <kriatiff@list.ru>
2022-11-09 12:53:44 +0300

    Выполнил ДЗ к 5-му уроку (что смог).

---
+++ Lesson_5_task_4_GavrilovGY.py
@@ -0,0 +1,32 @@
+# 4) Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
+
+with open('code.txt', 'r') as code:
+    code = code.read()
+
+def coding(txt):
+    count = 1
+    res = ''
+    for i in range(len(txt)-1):
+        if txt[i] == txt[i+1]:
+            count += 1
+        else:
+            res = res + str(count) + txt[i]
+            count = 1
+    if count > 1 or (txt[len(txt)-2] != txt[-1]):
+        res = res + str(count) + txt[-1]
+    return res
+
+def decoding(txt):
+    number = ''
+    res = ''
+    for i in range(len(txt)):
+        if not txt[i].isalpha():
+            number += txt[i]
+        else:
+            res = res + txt[i] * int(number)
+            number = ''
+    return res
+
+s = code
+print(f"Текст после кодирования: {coding(s)}")
+print(f"Текст после декодирования: {decoding(coding(s))}")
